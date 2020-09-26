86fc19d6417788687844d7943e3a29d1933965d8
VanesaYanet <vanesa22yanet@gmail.com>
2020-09-26 18:30:41 -0300

    Commit Prueba Resolución de Conflictos

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,6 @@
 public class Prueba {
 	public static void main(String [] args) {
-	Persona p = new Persona("vAlyx");
+	Persona p = new Persona("Mamani");
 	System.out.println("nombre: "+p.getNombre()); 
 	}
 }
