a0b87791f9aacd446bfcc2b0bb5044646b5a89f6
VanesaYanet <vanesa22yanet@gmail.com>
2020-09-26 19:27:06 -0300

    Commit Segunda fase Prueba Resolución de Conflictos

--- Prueba.java
+++ Prueba.java
@@ -1,6 +1,6 @@
 public class Prueba {
 	public static void main(String [] args) {
-	Persona p = new Persona("vAlyx");
+	Persona p = new Persona("Vanesa");
 	System.out.println("nombre: "+p.getNombre()); 
 	}
 }
